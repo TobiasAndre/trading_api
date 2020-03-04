e847a1de081ab39a89bd9191439bf5058276f493
TOBIAS ANDRE EGGERS <tobias_andre@icloud.com>
2020-03-04 20:16:21 -0300

    changing docker compose and start script for better deploy

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
+echo 'waiting database start...'
+sleep 30
+echo 'starting bundle proccess'
 # Gem install
 bundle check || bundle install
-# creating a database and pending migrations
-bundle exec rails db:create || bundle exec rails db:migrate
+# creating a database
+bundle exec rails db:create
+# run pending migrations
+bundle exec rails db:migrate
 # run tests
 bundle exec rspec
-# importing data from csv files
+# import data from csv files
 bundle exec rails db:seed
 # Run our server
 bundle exec puma -C config/puma.rb
